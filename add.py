d6e7a1c23fbe36bf25b39f48d81c3ce3dd1f25e1
ANET ELIZA JOHNY <44517@fisat>
2019-05-15 05:20:32 -0400

    Revert "create a function to add 4 numbers"
    This reverts commit 44d6efbbc7e49db72434918a0e462d72e6a05cdb.

--- add.py
+++ add.py
@@ -4,11 +4,6 @@ def add2(num1,num2)
 def add3(num1,num2,num3)
 	s=num1+num2+num3
 	return s
-def add4(num1,num2,num3,num4)
-	s=num1+num2+num3+num4
-	return s
 print add2(1,5)
 print add3(3,5,4)
-print add4(2,5,3,6)
-
 
